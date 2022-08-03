4757eae157333a3b813a227b4fa9578ca6a0081b
johnkimaiyo <kimaiyojohn6@gmail.com>
2022-08-03 08:12:11 +0300

    parseint

---
+++ Javascript Parsing Numbers/parseInt()/parseint 1.js
@@ -0,0 +1,59 @@
+//example 1
+let x = 5;
+let y = "10";
+let yInt = Number.parseInt(y);
+console.log(x + yInt);
+
+// examaple 2
+let a = 10;
+let b = "15";
+let bInt = Number.parseInt(b);
+console.log(a + bInt);
+
+// example 3
+let c = 15;
+let d = "20";
+let cInt = Number.parseInt(c);
+console.log(c + cInt);
+
+// example 4
+let e = 20;
+let f = "25";
+let fInt = Number.parseInt(f);
+console.log(e + fInt);
+
+// example 5
+let g = 25;
+let h = "30";
+let iInt = Number.parseInt(f);
+console.log(g + iInt);
+
+// exmple 6
+let i = 30;
+let j = "35";
+let jInt = Number.parseInt(j);
+console.log(i + jInt);
+
+// example 7
+let k = 35;
+let l = "40";
+let mInt = Number.parseInt(l);
+console.log(k + mInt);
+
+// example 8
+let m = 40;
+let n = "45";
+let oInt = Number.parseInt(n);
+console.log(m + oInt);
+
+// example 9
+let p = 45;
+let q = "45";
+let rInt = Number.parseInt(q);
+console.log(p + rInt);
+
+// example 10
+let s = 45;
+let t = "50";
+let uInt = Number.parseInt(t);
+console.log(s + uInt);
